c538b03158874bf7af8fc8f5fce196d877b2e361
Natalya7oct <33576808+Natalya7oct@users.noreply.github.com>
2024-04-29 21:57:09 +0300

    Update get_datasets.py

--- get_datasets.py
+++ get_datasets.py
@@ -34,7 +34,7 @@ from sklearn.metrics import precision_recall_curve
 import matplotlib.pyplot as plt
 import requests
 
-def write_formated_data_coco(data_path, captions, instances):
+def write_formated_data_coco(args, captions, instances):
 
   data = pd.DataFrame(captions['images'])
   captions = pd.DataFrame(captions['annotations'])
@@ -77,22 +77,22 @@ def write_formated_data_coco(data_path, captions, instances):
   lines_val = lines[int(len(lines)*args.train_perc):int(len(lines)*args.train_perc)+int(len(lines)*args.val_perc)]
   lines_test = lines[int(len(lines)*args.train_perc)+int(len(lines)*args.val_perc):]
 
-  with open(f'{data_path}/train.jsonl', 'w') as f:
+  with open(f'{args.data_path_coco}/train.jsonl', 'w') as f:
       for item in lines_train:
           f.write(json.dumps(item) + "\n")
 
-  with open(f'{data_path}/val.jsonl', 'w') as f:
+  with open(f'{args.data_path_coco}/val.jsonl', 'w') as f:
       for item in lines_val:
           f.write(json.dumps(item) + "\n")
 
-  with open(f'{data_path}/test.jsonl', 'w') as f:
+  with open(f'{args.data_path_coco}/test.jsonl', 'w') as f:
       for item in lines_test:
           f.write(json.dumps(item) + "\n")
 
-def write_formated_data_mmimdb(data_path):
+def write_formated_data_mmimdb(args):
 
   id_list = []
-  for file in sorted(os.listdir(f'{data_path}/dataset')):
+  for file in sorted(os.listdir(f'{args.data_path_mmimdb}/dataset')):
     id_list.append(file.split('.')[0])
   id_list = list(set(id_list))
   len_id_list = len(id_list)
@@ -104,7 +104,7 @@ def write_formated_data_mmimdb(data_path):
   train_labels = set()
   train = []
   for idx in id_list_train:
-      data = json.load(open(f'{data_path}/dataset/{idx}.json'))
+      data = json.load(open(f'{args.data_path_mmimdb}/dataset/{idx}.json'))
       train.append(
           {
               'label': data['genres'],
@@ -114,13 +114,13 @@ def write_formated_data_mmimdb(data_path):
       )
       for label in data['genres']:
           train_labels.add(label)
-  with jsonlines.open(f'{data_path}/train.jsonl', 'w') as writer:
+  with jsonlines.open(f'{args.data_path_mmimdb}/train.jsonl', 'w') as writer:
     writer.write_all(train)
 
 
   val = []
   for idx in id_list_val:
-      data = json.load(open(f'{data_path}/dataset/{idx}.json'))
+      data = json.load(open(f'{args.data_path_mmimdb}/dataset/{idx}.json'))
       label = [label for label in data['genres'] if label in train_labels]
       if len(label)>0:
           val.append(
@@ -130,12 +130,12 @@ def write_formated_data_mmimdb(data_path):
                   'text': data['plot'][0]
               }
           )
-  with jsonlines.open(f'{data_path}/val.jsonl', 'w') as writer:
+  with jsonlines.open(f'{args.data_path_mmimdb}/val.jsonl', 'w') as writer:
     writer.write_all(val)
 
   test = []
   for idx in id_list_test:
-      data = json.load(open(f'{data_path}/dataset/{idx}.json'))
+      data = json.load(open(f'{args.data_path_mmimdb}/dataset/{idx}.json'))
       label = [label for label in data['genres'] if label in train_labels]
       if len(label)>0:
           test.append(
@@ -145,5 +145,5 @@ def write_formated_data_mmimdb(data_path):
                   'text': data['plot'][0]
               }
           )
-  with jsonlines.open(f'{data_path}/test.jsonl', 'w') as writer:
+  with jsonlines.open(f'{args.data_path_mmimdb}/test.jsonl', 'w') as writer:
     writer.write_all(test)
